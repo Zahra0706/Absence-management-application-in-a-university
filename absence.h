e79a4cae184cfa59d8da4c5c0caee5d002ec7c7e
aya20053 <129607003+aya20053@users.noreply.github.com>
2023-05-02 23:11:56 +0100

    Add files via upload

---
+++ absence.h
@@ -0,0 +1,20 @@
+#ifndef ABSENCE_H
+#define ABSENCE_H
+
+#include "Etudiant.h"
+#include "Cours.h"
+
+class Absence {
+private:
+    std::string date;
+    Etudiant* etudiant;
+    Cours* cours;
+
+public:
+    Absence(std::string date, Etudiant* etudiant, Cours* cours);
+    std::string getDate() const;
+    Etudiant* getEtudiant() const;
+    Cours* getCours() const;
+};
+
+#endif 
